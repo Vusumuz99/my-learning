51b6c78d79b2de75bd0c4f35d74285a72e7264aa
unknown <mpanza.vusi5@gmail.com>
2021-11-09 15:26:26 +0200

    ol

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-var names = ["BMW","Toyoat","Opel","leux","Hando","Volkswagen","Smart","Suzuki","Subaru","RAM","Seat","Nissan","Mazda","Mercedes-Benz"];
+const names = ["BMW","Toyoat","Opel","leux","Hando",
+"Volkswagen","Smart","Suzuki","Subaru","RAM","Seat","Nissan","Mazda","Mercedes-Benz"]
+
+
+const display = document.getElementById("display");
+
+for (let i = 0; i < names.length; i++) {
+    display.innerHTML += "<li>" + names [i]
+    
+}
